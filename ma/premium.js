6486690e065e8dbd68ea76e1c38dcb0c6d17e30f
intelligent17-oss <amayointelligent17@gmail.com>
2025-11-19 01:21:01 +0100

    Create premium.js

---
+++ ma/premium.js
@@ -0,0 +1,11 @@
+// premium.js
+// List of premium Telegram user IDs (string values).
+// Add new user IDs here when someone pays and is approved by admin.
+
+const premiumUsers = [
+  "6976365864",
+    "000",
+"6940101627",
+"7979664801"
+  // your Admin ID is premium by default
+];
