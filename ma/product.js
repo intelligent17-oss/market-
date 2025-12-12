abc26572c97b4ab130778c04dd5696a0090c12dd
intelligent17-oss <amayointelligent17@gmail.com>
2025-12-12 19:07:07 +0100

    Update product.js

--- ma/product.js
+++ ma/product.js
@@ -1,6 +1,17 @@
 // PRODUCTS DATA
 const productsData = [
 {
+  id: "P1765562740044",
+  name: "Plenty TikTok account",
+  description: "Plenty TikTok account",
+  price: "10000",
+  currency: "NGN",
+  image: "https://i.ibb.co/PZYVGVq0/Screenshot-20251211-125529.png",
+  sellerId: 6976365864,
+  sellerContact: "+2348121697423",
+  url: "https://i.ibb.co/PZYVGVq0/Screenshot-20251211-125529.png"
+},
+{
   id: "P1765523341865",
   name: "2023 Sharp Facebook Switch Profile",
   description: "Already secured and active with 100 percent bombing response, text me for login",
