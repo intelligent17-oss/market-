83bb3c5dbd23977f22db7c7385305d05825ed870
intelligent17-oss <amayointelligent17@gmail.com>
2025-12-13 06:27:28 +0100

    Update balance.js

--- ma/balance.js
+++ ma/balance.js
@@ -15,7 +15,7 @@ const walletBalances = {
 
   // ===== SELLERS / USERS =====
   "6976365864": 1000,    // ₦25,000
-  "7979664801": 2000,     // ₦52,200
+  "7979664801": 2000,     // ₦2,200
 
   // Add more users below 👇
   // "TELEGRAM_ID": BALANCE_IN_NGN,
