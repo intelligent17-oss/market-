257dc17a41039acea0b8131060b086dbdbf9a902
intelligent17-oss <amayointelligent17@gmail.com>
2025-12-15 03:27:24 +0100

    Create pproduct.js

---
+++ ma/pproduct.js
@@ -0,0 +1,14 @@
+// PRODUCTS DATA
+const productsData = [
+{
+  id: "P1765562740044",
+  name: "Plenty TikTok account",
+  description: "Plenty TikTok account",
+  price: "10000",
+  currency: "NGN",
+  image: "https://i.ibb.co/PZYVGVq0/Screenshot-20251211-125529.png",
+  sellerId: 6976365864,
+  sellerContact: "+2348121697423",
+  url: "https://i.ibb.co/PZYVGVq0/Screenshot-20251211-125529.png"
+},
+];
