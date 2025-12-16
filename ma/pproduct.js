0351981ff97de46ef93e732562b02329a503fbfd
intelligent17-oss <amayointelligent17@gmail.com>
2025-12-16 05:03:46 +0100

    Update pproduct.js

--- ma/pproduct.js
+++ ma/pproduct.js
@@ -11,4 +11,5 @@ const productsData = [
   sellerContact: "+2348121697423",
   url: "https://i.ibb.co/PZYVGVq0/Screenshot-20251211-125529.png"
 },
+{ id:"P176399696094362", name:"Facebook", description:"Good & Active.", price:"7000", currency:"NGN", image:"https://www.facebook.com/profile.php?id=61582947623474", sellerId: 8111362786, sellerContact: "Whatsapp: +2349166314650", url:"https://www.facebook.com/profile.php?id=61582947623474" },
 ];
